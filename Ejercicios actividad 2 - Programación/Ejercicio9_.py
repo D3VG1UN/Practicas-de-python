b790d0069345c4d9f305e432975bfddef0d4c0c7
EricCasals <eric.casals@campus.xaloc.org>
2025-09-23 10:00:14 +0200

    Cambio archivo - 10 y 11

---
+++ Ejercicios actividad 2 - Programación/Ejercicio9_.py
@@ -0,0 +1,13 @@
+#Programa que pida los segundos y muestre por pantalla y en la misma frase los minutos 
+#y las horas
+
+segundos=float(input("Introduce los segundos que quieres convertir: "))
+
+horas=round(segundos/3600, 2)
+minutos=segundos/60
+
+print(f"El número de minutos es: {minutos} y de horas es: {horas}")
+<<<<<<< HEAD
+print(f"El número de minutos es: {minutos} y de horas es: {horas}")
+=======
+>>>>>>> c644f9890a4d3586fb6d439c8d2c76b652e10362
