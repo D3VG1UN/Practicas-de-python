f5ecd0a0f4e101bbde518fa263356ad42535af7e
EricCasals <eric.casals@campus.xaloc.org>
2025-09-24 19:06:42 +0200

    Update Ejercicio18.py

--- Ejercicios actividad 2 - Programación/Ejercicio18.py
+++ Ejercicios actividad 2 - Programación/Ejercicio18.py
@@ -3,4 +3,19 @@
 #de 18 años un 50%. Si la entrada cuesta 12 euros, calcula el total a pagar introduciendo por 
 #teclado el número de menores y el número de adultos que asisten al cine.
 
-Niños=int(input("Introduce cuántos niños menores de 18 años asisten: "))
+Niños=float(input("Introduce cuántos niños menores de 18 años asisten: "))
+Adultos=float(input("Ahora introduce la cantidad de adultos que asisten: "))
+
+RebajaNiños=(50/100)*12
+PrecioNiños=12-RebajaNiños
+PrecioMenores=round(PrecioNiños*Niños, 1)
+
+RebajaAdultos=(10/100)*12
+PrecioAdultos=12-RebajaAdultos
+PrecioFinalA=round(PrecioAdultos*Adultos, 1)
+
+Total=PrecioFinalA+PrecioMenores
+
+print("El total a pagar del/los menor/es es de:", PrecioMenores)
+print("El total a pagar del/los adulto/s es de:", PrecioFinalA)
+print("El total a pagar es:", Total)
