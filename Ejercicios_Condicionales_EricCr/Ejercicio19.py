0f025f889e9ede25317286a060e43ad3a963f37c
EricCasals <eric.casals@campus.xaloc.org>
2025-09-26 15:34:40 +0200

    Nuevo condicionales

---
+++ Ejercicios_Condicionales_EricCr/Ejercicio19.py
@@ -0,0 +1,13 @@
+#Programa que introduzca dos números y devuelva cuál de los dos es mayor, menor o son 
+#iguales
+
+num1=int(input("Introduce el primer número: "))
+num2=int(input("Introduce el segundo número: "))
+
+if num1 > num2:
+    print(f"El número {num1} es mayor que el número {num2}")
+else:
+    if num2 > num1:
+        print(f"El número {num2} es mayor que el número {num1}")
+    else:
+        print(f"El número {num1} y el número {num2} son iguales")
