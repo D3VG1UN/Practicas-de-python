a2d48f2180efd2f4fe9ed8f44545ba9c8cc42b3c
EricCasals <eric.casals@campus.xaloc.org>
2025-09-26 15:48:37 +0200

    Create Ejercicio20.py

---
+++ Ejercicios_Condicionales_EricCr/Ejercicio20.py
@@ -0,0 +1,16 @@
+#A partir del ejercicio anterior, forzar que el usuario solo pueda introducir por teclados 
+#números entre 0 y 10
+
+num1=int(input("Introduce el primer número entre 1 y 10: "))
+num2=int(input("Introduce el segundo número entre 1 y 10: "))
+
+if 1 <= num1 <= 10 and 1 <= num2 <= 10:
+    if num1 > num2:
+        print(f"El número {num1} es mayor que el número {num2}")
+    else:
+        if num2 > num1:
+            print(f"El número {num2} es mayor que el número {num1}")
+        else:
+            print(f"El número {num1} y el número {num2} son iguales")
+else:
+    print("Uno o los dos números están fuera de los límites establecidos")
