d6fbd9ff16361f60c8b496e5758715c60c04df69
EricCasals <eric.casals@campus.xaloc.org>
2025-09-26 16:04:44 +0200

    Update Ejercicio21.py

--- Ejercicios_Condicionales_EricCr/Ejercicio21.py
+++ Ejercicios_Condicionales_EricCr/Ejercicio21.py
@@ -1,2 +1,14 @@
 #Programa que calcula una ecuación de segundo grado. Controla que el valor de la raíz 
 #cuadrada no de un valor negativo
+import math
+
+valorA=float(input("Introduce el valor de a para la ecuación: "))
+valorB=float(input("Introduce el valor de b para la ecuación: "))
+valorC=float(input("Introduce el valor de c para la ecuación: "))
+
+Discriminante=valorB**2-4*valorA*valorC
+
+if Discriminante > 0:
+    x1=(-valorB+math.sqrt(Discriminante))/(2*valorA)
+    x2=(-valorB-math.sqrt(Discriminante))/(2*valorA)
+    print(f"El valor de x1 es {x1} y el valor de x2 es {x2}")
