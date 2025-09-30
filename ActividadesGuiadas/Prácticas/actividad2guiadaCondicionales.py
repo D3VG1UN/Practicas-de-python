68118e29a683857987edebf1dea480f7a94fc3bd
EricCasals <eric.casals@campus.xaloc.org>
2025-09-30 10:46:29 +0200

    Update actividad2guiadaCondicionales.py

--- ActividadesGuiadas/Prácticas/actividad2guiadaCondicionales.py
+++ ActividadesGuiadas/Prácticas/actividad2guiadaCondicionales.py
@@ -12,22 +12,3 @@ if var1 == var2:
     print("Las dos variables son iguales")
 else:
     print("Las dos variables son diferentes")
-
-#Crear un menú que te deje escoger entre las siguientes opciones:
-
-Escogencia=int(input("Escoge un plato en el siguiente menú (introduce el número): "))
-print("1. hamburguesa")
-print("2. pizza")
-print("3. lentejas")
-print("4. bocadillo de jamón")
-
-if Escogencia == 1:
-    print("Te serviremos una hamburguesa")
-elif Escogencia == 2:
-    print("Te serviremos una pizza")
-elif Escogencia == 3:
-    print("Te serviremos unas lentejas")
-elif Escogencia == 4:
-    print("te traeremos un bocadillo de jamón")
-else:
-    print("Creo que se ha equivocado de plato. Por favor, vuelva a pedir")
