7c1a3e5648e139c7a7e882160465c7ffde88a3aa
EricCasals <eric.casals@campus.xaloc.org>
2025-10-01 09:10:44 +0200

    and, or y not

--- ActividadesGuiadas/Prácticas/actividad2guiadaCondicionales.py
+++ ActividadesGuiadas/Prácticas/actividad2guiadaCondicionales.py
@@ -4,6 +4,11 @@
 #en python, después de un "if", se añade una tabulación, donde se escribe el código
 
 #Introducir un valor por teclado y que te diga si son iguales. Ejercicio de práctica.
+#Se pueden introducir las palabras and, or y not para poner más de una variable
+
+#Por ejemplo: if var==2 or var==4:
+#O: if var==3 and var1==6:
+#if not op==op1:
 
 var1=int(input("Introduce un valor: "))
 var2=int(input("Introduce otro valor: "))
