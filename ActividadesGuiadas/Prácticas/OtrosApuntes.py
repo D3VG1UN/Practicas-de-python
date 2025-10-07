2d17b5c42fe3871720bf9cce984f91dc507258ac
EricCasals <eric.casals@campus.xaloc.org>
2025-10-07 10:46:54 +0200

    Apuntes

--- ActividadesGuiadas/Prácticas/OtrosApuntes.py
+++ ActividadesGuiadas/Prácticas/OtrosApuntes.py
@@ -5,3 +5,18 @@
 var1,var2,var3="hello world",2,"19"
 
 print(var1, var2, var3)
+
+#Para usar los métodos string, el texto TIENE que ser un texto. Por lo tanto, debemos usar texto en vez de números.
+#Si después necesitamos un número, podemos reasignar un estado de variable. Por ejemplo:
+import math
+
+diametro=input("Introduce el diametro: ")
+if diametro.isnumeric() and int(diametro)>=1 and int(diametro)<=10:
+    diametro=int(diametro)
+    radio=diametro/2
+    area=math.pi*radio**2
+    perimetro=2*(math.pi*radio)
+    print(round(area,2))
+
+#Después de este código, añadimso código con operador lógico para comprobar si el número está entre 1 y 10
+#                                            (and, or y not)
---
+++ Ejercicios_Condicionales_EricCr/Ejercicio33.py
@@ -0,0 +1,7 @@
+#Programa un código que permita contar el número de vocales de la siguiente frase: No 
+#hay mal que dure cien años
+
+frase=str("No hay mal que dure 100 años")
+vocales=("a", "e", "i", "o", "u", "á", "é", "í", "ó", "ú")
+
+print(vocales)
