f33e885fd5290a161a5020a4a28eb0fe3033736b
EricCasals <eric.casals@campus.xaloc.org>
2025-10-07 10:08:01 +0200

    Create Practica string.py

---
+++ ActividadesGuiadas/Prácticas/Practica string.py
@@ -0,0 +1,9 @@
+txt = "I love apples, apple Apple my favourite fruit"
+todominuscula = txt.lower()
+#print(todominuscula)
+
+palabra=input("Introduce una palabra: ")
+
+x = todominuscula.count(palabra)
+
+print(x)
