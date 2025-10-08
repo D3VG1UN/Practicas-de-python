ec22ae74820e6541c06c76b93468dc41985a5469
EricCasals <eric.casals@campus.xaloc.org>
2025-10-08 09:54:00 +0200

    Update Ejercicio34.py

---
+++ Ejercicios_Condicionales_EricCr/Ejercicio34.py
@@ -0,0 +1,16 @@
+#Corrige los 4 errores o añade el código que necesites para que el siguiente programa se 
+#ejecute correctamente
+
+#inicializo valores a cada variable
+var_numero=int(6734)
+var_suma=int(0)
+#obtengo su longitud
+var_longitud=len(var_numero)
+#sumo cada digito a partir del índice de cada posición
+var_suma = var_numero [1] + var_numero [2]+ var_numero[3] + var_numero [4]
+#utilizo una condición y el operador aritmético // para saber si el resto da 0 y ver si es par
+if var_suma // 2 == 0:
+    print (f"el valor de {var_suma} es impar")
+
+#Error 1 (que he encontrado): tiene que haber un tab después del if
+#Error 2: valta establecer las variables como int
