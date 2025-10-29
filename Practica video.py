722c2d5bff7a805073c8abea9a9c6430d823d4ab
EricCasals <eric.casals@campus.xaloc.org>
2025-10-29 09:47:51 +0100

    Update Practica video.py

--- Practica video.py
+++ Practica video.py
@@ -3,7 +3,9 @@ from tkinter import *
 root=Tk()
 
 #creating a Label widget
-myLabel = Label(root, text="Hello World")
+myLabel = Label(root, text="Hello World!")
 
 #Shoving it onto the screen
 myLabel.pack()
+
+root.mainloop()
