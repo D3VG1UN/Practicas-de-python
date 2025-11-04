3002b4dc813380c7fe78cc193710f876a08709ef
EricCasals <eric.casals@campus.xaloc.org>
2025-11-04 10:28:04 +0100

    Create Práctica bucles for.py

---
+++ Bucles for/Práctica bucles for.py
@@ -0,0 +1,8 @@
+#práctica con los bucles for para comprobar si el valor de una variable introducida está en un rango de número
+
+#prueba=int(input("Introduce un valor numérico entero por pantalla: "))
+#pruebastring=str(input("Introduce una frase o una palabra: "))
+#frase="Cuando digo digo, digo diego"
+
+for prueba in range(1,1001):
+    print(prueba)
