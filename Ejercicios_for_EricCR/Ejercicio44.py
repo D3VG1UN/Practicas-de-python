1ddcd2a5ca83f6945ec4c63c103ebb528e55d396
EricCasals <eric.casals@campus.xaloc.org>
2025-11-11 10:47:55 +0100

    Create Ejercicio44.py

---
+++ Ejercicios_for_EricCR/Ejercicio44.py
@@ -0,0 +1,6 @@
+#44. Realiza un programa que recorra todos los números comprendidos de 0 a 100 realizando saltos 
+#de 3 en 3. El resultado debe aparecer por pantalla en una línea con los números separados por ‘,’
+i=0
+for x in range (34):
+    print(i, end=",")
+    i=i+3
