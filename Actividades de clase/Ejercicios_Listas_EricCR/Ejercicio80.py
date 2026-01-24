8f79e81da1c42555e40805c3ba7cf7870f72f150
EricCasals <eric.casals@campus.xaloc.org>
2026-01-24 13:23:23 +0100

    Create Ejercicio80.py

---
+++ Actividades de clase/Ejercicios_Listas_EricCR/Ejercicio80.py
@@ -0,0 +1,7 @@
+#80. Utilizando listas, crea un programa que te permita determinar si un número es decimal o no. 
+import string
+
+lista1=[]
+num=list(input("Introduce un número"))
+lista1.append()
+num.split()
