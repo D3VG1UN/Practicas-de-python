60c58d7569d54660bbfef75648795c73e2360d3a
EricCasals <eric.casals@campus.xaloc.org>
2026-01-27 08:55:16 +0100

    Update Ejercicio80.py

--- Actividades de clase/Ejercicios_Listas_EricCR/Ejercicio80.py
+++ Actividades de clase/Ejercicios_Listas_EricCR/Ejercicio80.py
@@ -1,7 +1,22 @@
 #80. Utilizando listas, crea un programa que te permita determinar si un número es decimal o no. 
 import string
 
-lista1=[]
-num=list(input("Introduce un número"))
-lista1.append()
-num.split()
+num=list(input("Introduce un número "))
+punt=".,"
+decimal=1
+letra=""
+
+for x in num:
+    if x in punt:
+        decimal=1
+    elif x.isalpha():
+        letra="Si"
+    else:
+        decimal=0
+
+if decimal==1 and not letra=="Si":
+    print("El número es decimal")
+if decimal==0:
+    print("No es un número decimal")
+
+#No sé como hacerlo y no encuentro el error
