7a3795e12f17fa718061032146e5ecc717213ade
EricCasals <eric.casals@campus.xaloc.org>
2026-02-03 09:31:06 +0100

    Create Ahorcado.py

---
+++ Ahorcado.py
@@ -0,0 +1,9 @@
+#ejercicio del ahorcado del Sway
+import random
+
+Lista_palabrasecreta=["Guatamalteco", "Alcantarillado", "Imaginación", "Trauma", "Wok", "Silenciador","Centrifugación","Pudrimiento", "Nueve", "Movimiento"]
+palabrasecreta=Lista_palabrasecreta[random.randint(0, 9)]
+print(palabrasecreta)
+Lista_partida=[]
+
+Lista_ahorcado=[]
