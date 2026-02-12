e6b0c3d2a59220e878e87b5c238c486263b100cb
EricCasals <eric.casals@campus.xaloc.org>
2026-02-12 15:42:30 +0100

    hhhbjhvbvgghhghc

--- EjerciciosJutge/EjerciciosXaloc/P34279.py
+++ EjerciciosJutge/EjerciciosXaloc/P34279.py
@@ -2,8 +2,17 @@
 
 num=input()
 listanums=num.split()
-h=listanums[0]
-m=listanums[1]
+h=int(listanums[0])
+m=int(listanums[1])
 s=int(listanums[2])+1
 
-print(f"{h}:{m}:{s}")
+if s == 60:
+    s = 0
+    m += 1
+    if m == 60:
+        m = 0
+        h += 1
+        if h == 24:
+            h = 0
+#esto me lo enseñó Mangel una vez, así que he decidido intentar volver a aplicarlo
+print((("0" + str(h)) if h < 10 else str(h)) + ":" + (("0" + str(m)) if m < 10 else str(m)) + ":" +(("0" + str(s)) if s < 10 else str(s)))
--- EjerciciosJutge/EjerciciosXaloc/P37469.py
+++ EjerciciosJutge/EjerciciosXaloc/P37469.py
@@ -4,5 +4,10 @@ n=int(input())
 
 
 h=n//3600
+r=n%3600
+m=r//60
+r2=n%60
+s=r2
 
+print(h, m, s)
 #incompleto
