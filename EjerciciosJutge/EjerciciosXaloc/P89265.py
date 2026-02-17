2a1949610d4e9772fc5f462691f882d776452a71
EricCasals <eric.casals@campus.xaloc.org>
2026-02-17 10:25:26 +0100

    Add files via upload

---
+++ EjerciciosJutge/EjerciciosXaloc/P89265.py
@@ -0,0 +1,71 @@
+#ejercicio 22. Intervalos III.
+
+intervalos=input()
+listaintervalos=intervalos.split()
+
+num1=int(listaintervalos[0])
+num2=int(listaintervalos[1])
+num3=int(listaintervalos[2])
+num4=int(listaintervalos[3])
+
+salida=[]
+
+if num2<num3:
+    salida=[]
+elif num4<num1:
+    salida=[]
+
+else:
+    if num1>num3:
+        if num4>num2:
+            salida.append(num1)
+            salida.append(num2)
+
+        if num4<num2:
+            salida.append(num1)
+            salida.append(num4)
+
+        if num4==num2:
+            salida.append(num1)
+            salida.append(num2)
+
+    elif num1<num3:
+        if num4>num2:
+            salida.append(num3)
+            salida.append(num2)
+
+        if num2>num4:
+            salida.append(num3)
+            salida.append(num4)
+
+        if num2==num4:
+            salida.append(num3)
+            salida.append(num2)
+
+    elif num1==num3:
+        if num4>num2:
+            salida.append(num3)
+            salida.append(num2)
+
+        if num2>num4:
+            salida.append(num3)
+            salida.append(num4)
+
+        if num2==num4:
+            salida.append(num3)
+            salida.append(num2)
+
+    else:
+        salida=[]
+
+salida = str(salida).replace(" ", "")
+
+if num1==num3 and num2==num4:
+    print("= ,",salida)
+else:
+    if num2<=num4 and num1>=num3:
+        print(1,",",salida)
+    elif num2>=num4 and num1<=num3:
+        print(2,",",salida)
+    else:
+        print("? ,",salida)
