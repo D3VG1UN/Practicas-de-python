a98a4948763a23194c55d52aaf7ef4892d3b248a
EricCasals <eric.casals@campus.xaloc.org>
2026-02-17 10:46:05 +0100

    Add files via upload

--- Ahorcado.py
+++ Ahorcado.py
@@ -5,5 +5,21 @@ Lista_palabrasecreta=["Guatamalteco", "Alcantarillado", "Imaginación", "Trauma"
 palabrasecreta=Lista_palabrasecreta[random.randint(0, 9)]
 print(palabrasecreta)
 Lista_partida=[]
-
 Lista_ahorcado=[]
+Palabrasecreta_lista=[]
+
+for i in palabrasecreta:
+    Lista_partida.append("_")
+    Palabrasecreta_lista.append(i)
+
+print(Lista_partida)
+
+for x in range (8):
+    letra=input("Intenta adivinar la letra: ")
+    if letra in palabrasecreta:
+        for n in range(len(palabrasecreta)):
+            if palabrasecreta[n]==letra:
+                Lista_partida[n]=letra
+                print("Una letra acertada")
+                print(Lista_partida)
+        
