3b1bc32f611995bc4c25c062a5fe571eaf115445
EricCasals <eric.casals@campus.xaloc.org>
2026-02-27 15:44:13 +0100

    Reogranización de carpetas

---
+++ Otros/Hola mundo - práctica con texto y variables.py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+
+var1=3
+var2=2
+total=3+2
+print(f"El resultado de la suma de {var1} + {var2} es:", total)
+
+nombre=input("Cuál es tu nombre?: ")
+print(f"Hola,{nombre}, mi nombre es D3V, ¡encantado de conocerte!")
+
